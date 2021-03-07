39afa996fc3244d2cf4e0ca5ea9fe313dab311c2
Amit Prajapati <amitgprajapati1998@gmail.com>
2021-03-08 00:18:24 +0530

    Completed Logic with UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react'
+import AddTask from './components/AddTask';
+const App = () =>{
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text:'Task 1',
+        day:'Feb 5th at 2:30pm',
+        reminder:true
+    },
+    {
+      id:2,
+      text:'Task 1',
+      day:'Feb 5th at 2:30pm',
+      reminder:true
+  }
+  ]);
 
-function App() {
+  const addTask = (task)=>{
+    const id = Math.floor(Math.random() *10000)+1;
+    const newTask ={id,...task};
+    setTasks([...tasks,newTask]);
+  }
+
+  const deleteTask = (id)=>{
+    setTasks(tasks.filter((tasks)=>tasks.id !== id))
+  }
+  const toggleReminder = (id)=>{
+    setTasks(tasks.map((task)=> task.id === id ? {...task, reminder:!task.reminder}:task))
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd = {showAddTask}/>
+      { showAddTask && <AddTask onAdd ={addTask}/>}
+      {tasks.length > 0 ?<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>:'No Task to Show'}
     </div>
   );
 }
